2ca3980cd9f898e6c2809aba4ef2c33c1d9f3102
Aviral Vishnoi <aviralvishnoi@Avirals-MacBook-Air.local>
2023-11-02 15:50:03 +0100

    updated notes for 21st october and 1st november

---
+++ 21-oct/assignment.md
@@ -0,0 +1,15 @@
+Create 2 lists instructors and students and populate names to these lists accordingly
+
+developers = ["Aviral", "Ram", "Bhanu", "Jatin", "Sudheer", "Kunal"]
+
+teams = ["India", "Australia", "Pakistan", "Netherlands", "Bangladesh", "Sri Lanka", "America", "Ireland"]
+
+teams_starting_with_vowels = []
+teams_starting_with_consonants = []
+
+"Teams starting with vowels are ?"
+"Teams starting with consonants are ?"
+
+players = ["vIrAt KOHli", "saChIn tenDuLKar", "suRabh ganGuLy"]
+
+create a new list in which the name of these players should be add with First Letter of their names as upper case and remaining letters as lowercase
---
+++ 21-oct/conditions.py
@@ -0,0 +1,89 @@
+# x =5 
+# y =10
+# print(f"Number are x ={x} and y = {y}")
+
+# z =x + y 
+
+# print(f"Output is = {z}")
+
+
+"""
+if condition:
+    you will do something
+elif condition:
+    do somthing
+elif condition:
+    do somthing
+else:
+
+"""
+
+# number = 10
+# if number > 20:
+#     print("Number is greater than 5")
+# else:
+#     print("Outside If!!")
+
+
+# percentage_marks = 86
+
+# if percentage_marks > 90 :
+#     print("Grade A")
+# elif percentage_marks >= 80 and percentage_marks < 90:
+#     print("Grade B")
+# elif percentage_marks >= 70 and percentage_marks < 80:
+#     print("Grade C")
+# elif percentage_marks >= 60 and percentage_marks < 70:
+#     print("Grade D")
+# else:
+#     print("Grade E")
+
+
+# y = 1
+
+# z = y**2 if y> 2 else y*10
+# print(z)
+
+# dummy_list = [2,3,4,5,6] 
+
+# if 13 not in dummy_list:
+#     print(13**2)
+
+
+"""
+
+*args
+**kwargs
+"""
+
+def get_grade(percentage_marks):
+    if percentage_marks > 90 :
+        print("Grade A")
+    elif percentage_marks >= 80 and percentage_marks < 90:
+        print("Grade B")
+    elif percentage_marks >= 70 and percentage_marks < 80:
+        print("Grade C")
+    elif percentage_marks >= 60 and percentage_marks < 70:
+        print("Grade D")
+    else:
+        print("Grade E")
+
+
+#get_grade(75)
+
+name = "Bhanu"
+comany = "Wipro"
+project = "dummy2"
+
+if name == "Bhanu" and comany == "Wipro" and project == "dummy":
+    print("I know bhanu!!")
+
+dummy_list = [2,3,4,5,6,7]
+
+for item in dummy_list:
+    if item %2 == 0:
+        print(item**2)
+    else:
+        print(item*10)
+
+
